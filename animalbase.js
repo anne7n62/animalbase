7ff3094225762a70b03c97c89a8cd16e0831d585
Annemette <anne7n62@stud.kea.dk>
2021-02-23 19:14:30 +0100

    added activity to sortby class

--- animalbase.js
+++ animalbase.js
@@ -98,6 +98,13 @@ function selectSort(event) {
     const sortBy = event.target.dataset.sort;
     const sortDir = event.target.dataset.sortDirection;
     
+    //find "old" sortby element, and remove .sortBy
+    const oldElement = document.querySelector(`[data-sort='${settings.sortBy}']`);
+    oldElement.classList.remove("sortby");
+
+    //indicate active sort
+    event.target.classList.add("sortby");
+
     // toggle the direction!
     if (sortDir === "asc") {
         event.target.dataset.sortDirection = "desc";
@@ -147,7 +154,7 @@ function buildList() {
 
     displayList (sortedList);
 }
-
+  
 function displayList(animals) {
     // clear the list
     document.querySelector("#list tbody").innerHTML = "";
